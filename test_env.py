b023d2c1a325e8b9ef54e5793a1b518c2a977408
Caesar16 <94758665+Caesar16@users.noreply.github.com>
2022-04-20 19:51:46 +0200

    Update test_env.py

--- test_env.py
+++ test_env.py
@@ -1,48 +1,25 @@
-import math
 import matplotlib.pyplot as plt
-import numpy as np
-
-function_inputs, desired_output = [], []
-# read data from the file
-with open('data_sinus.txt') as f:
-    for line in f:
-        function_inputs.append(float(line.split()[0]))
-        desired_output.append(float(line.split()[1]))
-
-diff_1, diff_2 = 0, 0
-for each in range(len(function_inputs)):
-    output = -3.15 * (math.sin(-4.97 * function_inputs[each]))
-    output_2 = -2.71 * (math.sin(-5.59 * function_inputs[each]))
-    diff_1 += abs(output - desired_output[each])
-    diff_2 += abs(output_2 - desired_output[each])
-    print(round(diff_1, 3), round(diff_2, 3))
-print(round(1.0 / diff_1, 3), round(1.0 / diff_2, 3))
-
-x = np.linspace(-math.pi, math.pi, 300)
-fx_1, fx_2, fx_3 = [], [], []
-for i in range(len(x)):
-    fx_1.append(-3.15*(math.sin(-4.97 * x[i])))
-    fx_2.append(3 * (math.sin(5 * x[i])))
-    fx_3.append(-2.71 * (math.sin(-5.59 * x[i])))
-
-fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-fig.suptitle('Wykresy funkcji z parametrami z epoki 2 i 7, na tle funkcji szukanej')
-ax1.plot(x, fx_3, x, fx_2)
-ax2.plot(x, fx_1, x, fx_2)
-plt.show()
 
 
-<<<<<<< HEAD
+def draw_chart(function_inputs, desired_output, count_output):
+    prc = 0
+    for each in range(len(function_inputs)):
+        if float(count_output[each]) > float(desired_output[each]):
+            prc = 100 - ((float(desired_output[each]) / float(count_output[each])) * 100)
+        else:
+            prc = 100 - ((float(count_output[each]) / float(desired_output[each])) * 100)
+    print("Error rate of fitness: {:.2f}%".format(prc))
+
+    fig, ax1 = plt.subplots(sharex=True)
+    fig.suptitle('Wykres funkcji zadanej do obliczonej')
+    ax1.plot(function_inputs, count_output, function_inputs, desired_output)
+    plt.show()
+
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+    fig.suptitle('Wykres funkcji w indywidualnych skalach')
+    ax1.plot(function_inputs, count_output)
+    ax2.plot(function_inputs, desired_output)
+    plt.show()
 
 
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> 88b6d222ee01adff0fd33b2373d1abc2ed1e0027
+draw_chart(function_inputs_DISPLACEMENTS, desired_output_FORCE, global_cache[solution_idx])
